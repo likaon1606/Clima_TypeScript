16aa61a14b64bb8638eefe3ae4fe5d674de648dd
likaon1606 <ariel.fuentesgarcia@gmail.com>
2024-12-05 15:14:28 -0600

    changes

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,36 +1,61 @@
+import { ChangeEvent, useState } from "react";
+import type { SearchType } from "../../types";
 import { countries } from "../../data/countries";
+import styles from './Form.module.css'
 
 export default function Form() {
-  return (
-    <form>
 
-      <div>
+  const [search, setSearch] =useState<SearchType>({
+    city: '',
+    country: ''
+  })
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => 
+  {
+    setSearch({
+      ...search,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  
+
+  return (
+    <form className={styles.form}>
+
+      <div className={styles.field}>
         <label htmlFor="city">Ciudad: </label>
         <input 
           id="city"
           type="text"
           name="city"
           placeholder="Ciudad"
-        
+          value={search.city}
+          onChange={handleChange}
         />
       </div>
 
-      <div>
+      <div className={styles.field}>
         <label htmlFor="country">País: </label>
-        <select name="" id="">
-        <option value="">-- Selecciona un País --</option>
-        {countries.map( country => (
-          <option
-            key={country.code}
-            value={country.code}
-          >
-            {country.name}
-          </option>
-        ))}
+        <select
+          id="country"
+          value={search.country}
+          name="country"
+          onChange={handleChange}
+        >
+          <option value="">-- Selecciona un País --</option>
+          {countries.map( country => (
+            <option
+              key={country.code}
+              value={country.code}
+            >
+              {country.name}
+            </option>
+          ))}
         </select>
       </div>
 
-      <input type="submit" value='Consultar Clima' />
+      <input className={styles.submit} type="submit" value='Consultar Clima' />
 
     </form>
   )
